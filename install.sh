5ded2e500ce18e49cfdacadb4c5c3987289b82d8
Vinnie Magro <v@vinnie.io>
2017-04-04 20:03:05 -0700

    Move vimrc to root

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ fi
 
 if [[ ! -a "$HOME/.vimrc" ]]; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-  ln -s "$DOTFILES/vim/vimrc" "$HOME/.vimrc"
+  ln -s "$DOTFILES/vimrc" "$HOME/.vimrc"
 fi
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe && ./install.py --clang-completer
