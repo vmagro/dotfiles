ba87582eae1ee31a3efd698234ae02a17db63015
Vinnie Magro <v@vinnie.io>
2017-04-04 17:28:10 -0700

    Basic vim setup

--- install.sh
+++ install.sh
@@ -6,4 +6,13 @@ pushd `dirname $0` > /dev/null
 DOTFILES=`pwd`
 popd > /dev/null
 
-ln -s "$DOTFILES/zsh/zshrc" "$HOME/.zshrc"
+if [[ ! -a "$HOME/.zshrc" ]]; then
+  ln -s "$DOTFILES/zsh/zshrc" "$HOME/.zshrc"
+fi
+
+if [[ ! -a "$HOME/.vimrc" ]]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  ln -s "$DOTFILES/vim/vimrc" "$HOME/.vimrc"
+fi
+vim +PluginInstall +qall
+cd ~/.vim/bundle/YouCompleteMe && ./install.py --clang-completer
