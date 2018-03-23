5e41f1fa60b5c8312aa83e7c2ce5bfc7dc46292e
Vinnie Magro <vmagro@fb.com>
2018-03-23 15:08:33 -0700

    vi mode in fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,8 @@ alias vim=nvim
 set EDITOR nvim
 set VISUAL nvim
 
+fish_vi_key_bindings
+
 set -x GOPATH $HOME/go
 set PATH /usr/local/sbin $PATH
 set PATH $HOME/bin $GOPATH/bin $PATH
--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,30 +1,30 @@
 function fish_prompt --description 'Write out the prompt'
 	# Save our status
-    set -l last_status $status
+  set -l last_status $status
 
-    set -l last_status_string ""
-    if [ $last_status -ne 0 ]
-        printf "%s(%d)%s " (set_color red --bold) $last_status (set_color normal)
-    end
+  set -l last_status_string ""
+  if [ $last_status -ne 0 ]
+      printf "%s(%d)%s " (set_color red --bold) $last_status (set_color normal)
+  end
 
-    set -l color_cwd
-    set -l suffix
-    switch $USER
-        case root toor
-            if set -q fish_color_cwd_root
-                set color_cwd $fish_color_cwd_root
-            else
-                set color_cwd $fish_color_cwd
-            end
-            set suffix '#'
-        case '*'
-            set color_cwd $fish_color_cwd
-            set suffix '›'
-    end
+  set -l color_cwd
+  set -l suffix
+  switch $USER
+      case root toor
+          if set -q fish_color_cwd_root
+              set color_cwd $fish_color_cwd_root
+          else
+              set color_cwd $fish_color_cwd
+          end
+          set suffix '#'
+      case '*'
+          set color_cwd $fish_color_cwd
+          set suffix '›'
+  end
 
-    echo -n -s (set_color $color_cwd) (prompt_pwd) " " (set_color normal)
+  echo -n -s (set_color $color_cwd) (prompt_pwd) " " (set_color normal)
 
-    __informative_git_prompt
+  __informative_git_prompt
 
-    echo -n (set_color green --bold) $suffix (set_color normal)
+  echo -n (set_color green --bold) $suffix (set_color normal)
 end
