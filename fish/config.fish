a0b20de9cb6f0a5c0d0991b59bf1f97090a0b0d8
Vinnie Magro <vmagro@fb.com>
2018-03-30 15:42:13 -0700

    Mess with PATH for some tools

--- fish/config.fish
+++ fish/config.fish
@@ -6,9 +6,15 @@ fish_vi_key_bindings
 
 set -x GOPATH $HOME/go
 set PATH /usr/local/sbin $PATH
-set PATH $HOME/bin $GOPATH/bin $PATH
+set PATH $HOME/bin $PATH
 
 alias :q=exit
+
+# golang at fb
+set -x GOPATH /data/users/$USER/gopaths/gothrift /data/users/$USER/gopaths/godeps /data/users/$USER/gopaths/gofbcode
+set -x GOROOT /data/users/$USER/gopaths/goroot
+set PATH /data/users/$USER/fbsource/fbcode/third-party-buck/gcc-5-glibc-2.23/tools/go-tools/bin $PATH
+set PATH /data/users/$USER/gopaths/goroot/bin $PATH
 
 # see comment in this file for the clowniness
 source ~/.config/fish/functions/fbcode_cd.fish
