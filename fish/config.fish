fdebc90b32d2cda397fc72a67e4ab659f85c5374
Vinnie Magro <vmagro@fb.com>
2018-05-01 14:51:56 -0400

    Add ~/.local/bin to PATH

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,7 @@ fish_vi_key_bindings
 set -x GOPATH $HOME/go
 set PATH /usr/local/sbin $PATH
 set PATH /var/lib/snapd/snap/bin $PATH
+set PATH $HOME/.local/bin $PATH
 set PATH $HOME/bin $PATH
 
 alias :q=exit
