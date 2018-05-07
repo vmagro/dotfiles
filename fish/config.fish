69c6b535522e850d1a17739d5a9aa7291e9dbe80
Vinnie Magro <vmagro@fb.com>
2018-05-07 12:47:23 -0700

    Fix path clowniness

--- fish/config.fish
+++ fish/config.fish
@@ -6,18 +6,25 @@ fish_vi_key_bindings
 
 set -x GOPATH $HOME/go
 set PATH /usr/local/sbin /sbin $PATH
-set PATH /var/lib/snapd/snap/bin $PATH
-set PATH $HOME/.local/bin $PATH
+if test -d /var/lib/snapd/snap/bin
+  set PATH /var/lib/snapd/snap/bin $PATH
+end
+if test -d $HOME/.local/bin
+  set PATH $HOME/.local/bin $PATH
+end
 set PATH $HOME/bin $PATH
 
 alias :q=exit
 
-# golang at fb
 # TODO: only enable this on fb devserver
-# set -x GOPATH "/data/users/$USER/gopaths/gothrift:/data/users/$USER/gopaths/godeps:/data/users/$USER/gopaths/gofbcode:$HOME/go/"
-# set -x GOROOT "/data/users/$USER/gopaths/goroot"
-# set PATH /data/users/$USER/fbsource/fbcode/third-party-buck/gcc-5-glibc-2.23/tools/go-tools/bin $PATH
-# set PATH /data/users/$USER/gopaths/goroot/bin $PATH
+set -l fb "true"
+# golang at fb
+if [ $fb ]
+   set -x GOPATH "/data/users/$USER/gopaths/gothrift:/data/users/$USER/gopaths/godeps:/data/users/$USER/gopaths/gofbcode:$HOME/go/"
+   set -x GOROOT "/data/users/$USER/gopaths/goroot"
+   set PATH /data/users/$USER/fbsource/fbcode/third-party-buck/gcc-5-glibc-2.23/tools/go-tools/bin $PATH
+   set PATH /data/users/$USER/gopaths/goroot/bin $PATH
+end
 
 # see comment in this file for the clowniness
 source ~/.config/fish/functions/fbcode_cd.fish
