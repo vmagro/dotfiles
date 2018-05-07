e943757201d2ef214b29497c84b04186901dc8ca
Vinnie Magro <vmagro@fb.com>
2018-05-07 15:09:53 -0400

    Fix theme path

--- awesome/themes/default/theme.lua
+++ awesome/themes/default/theme.lua
@@ -7,7 +7,7 @@ local xresources = require("beautiful.xresources")
 local dpi = xresources.apply_dpi
 
 local gfs = require("gears.filesystem")
-local themes_path = gfs.get_themes_dir()
+local theme_path = gfs.get_configuration_dir().."themes/default/"
 
 local theme = {}
 
@@ -62,7 +62,7 @@ theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
-theme.menu_submenu_icon = themes_path.."default/submenu.png"
+theme.menu_submenu_icon = theme_path.."submenu.png"
 theme.menu_height = dpi(15)
 theme.menu_width  = dpi(100)
 
@@ -72,55 +72,55 @@ theme.menu_width  = dpi(100)
 --theme.bg_widget = "#cc0000"
 
 -- Define the image to load
-theme.titlebar_close_button_normal = themes_path.."default/titlebar/close_normal.png"
-theme.titlebar_close_button_normal_hover = themes_path.."default/titlebar/close_normal_hover.png"
-theme.titlebar_close_button_normal_press = themes_path.."default/titlebar/close_normal_press.png"
-theme.titlebar_close_button_focus  = themes_path.."default/titlebar/close_focus.png"
-theme.titlebar_close_button_focus_hover = themes_path.."default/titlebar/close_focus_hover.png"
-theme.titlebar_close_button_focus_press = themes_path.."default/titlebar/close_focus_press.png"
+theme.titlebar_close_button_normal = theme_path.."titlebar/close_normal.png"
+theme.titlebar_close_button_normal_hover = theme_path.."titlebar/close_normal_hover.png"
+theme.titlebar_close_button_normal_press = theme_path.."titlebar/close_normal_press.png"
+theme.titlebar_close_button_focus  = theme_path.."titlebar/close_focus.png"
+theme.titlebar_close_button_focus_hover = theme_path.."titlebar/close_focus_hover.png"
+theme.titlebar_close_button_focus_press = theme_path.."titlebar/close_focus_press.png"
 
-theme.titlebar_minimize_button_normal = themes_path.."default/titlebar/minimize_normal.png"
-theme.titlebar_minimize_button_normal_hover = themes_path.."default/titlebar/minimize_normal_hover.png"
-theme.titlebar_minimize_button_normal_press = themes_path.."default/titlebar/minimize_normal_press.png"
-theme.titlebar_minimize_button_focus  = themes_path.."default/titlebar/minimize_focus.png"
-theme.titlebar_minimize_button_focus_hover = themes_path.."default/titlebar/minimize_focus_hover.png"
-theme.titlebar_minimize_button_focus_press = themes_path.."default/titlebar/minimize_focus_press.png"
+theme.titlebar_minimize_button_normal = theme_path.."titlebar/minimize_normal.png"
+theme.titlebar_minimize_button_normal_hover = theme_path.."titlebar/minimize_normal_hover.png"
+theme.titlebar_minimize_button_normal_press = theme_path.."titlebar/minimize_normal_press.png"
+theme.titlebar_minimize_button_focus  = theme_path.."titlebar/minimize_focus.png"
+theme.titlebar_minimize_button_focus_hover = theme_path.."titlebar/minimize_focus_hover.png"
+theme.titlebar_minimize_button_focus_press = theme_path.."titlebar/minimize_focus_press.png"
 
-theme.titlebar_maximized_button_normal_inactive = themes_path.."default/titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_normal_inactive_hover = themes_path.."default/titlebar/maximized_normal_inactive_hover.png"
-theme.titlebar_maximized_button_normal_inactive_press = themes_path.."default/titlebar/maximized_normal_inactive_press.png"
-theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_focus_inactive_hover = themes_path.."default/titlebar/maximized_focus_inactive_hover.png"
-theme.titlebar_maximized_button_focus_inactive_press = themes_path.."default/titlebar/maximized_focus_inactive_press.png"
+theme.titlebar_maximized_button_normal_inactive = theme_path.."titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_normal_inactive_hover = theme_path.."titlebar/maximized_normal_inactive_hover.png"
+theme.titlebar_maximized_button_normal_inactive_press = theme_path.."titlebar/maximized_normal_inactive_press.png"
+theme.titlebar_maximized_button_focus_inactive  = theme_path.."titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_focus_inactive_hover = theme_path.."titlebar/maximized_focus_inactive_hover.png"
+theme.titlebar_maximized_button_focus_inactive_press = theme_path.."titlebar/maximized_focus_inactive_press.png"
 
-theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_normal_active_hover = themes_path.."default/titlebar/maximized_normal_active_hover.png"
-theme.titlebar_maximized_button_normal_active_press = themes_path.."default/titlebar/maximized_normal_active_press.png"
-theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_focus_active_hover = themes_path.."default/titlebar/maximized_focus_active_hover.png"
-theme.titlebar_maximized_button_normal_active_press = themes_path.."default/titlebar/maximized_normal_active_press.png"
+theme.titlebar_maximized_button_normal_active = theme_path.."titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_normal_active_hover = theme_path.."titlebar/maximized_normal_active_hover.png"
+theme.titlebar_maximized_button_normal_active_press = theme_path.."titlebar/maximized_normal_active_press.png"
+theme.titlebar_maximized_button_focus_active  = theme_path.."titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_focus_active_hover = theme_path.."titlebar/maximized_focus_active_hover.png"
+theme.titlebar_maximized_button_normal_active_press = theme_path.."titlebar/maximized_normal_active_press.png"
 
 
--- theme.wallpaper = themes_path.."default/background.png"
-theme.wallpaper = "~/Pictures/Friends-Map-5120x2880.jpg"
+theme.wallpaper = theme_path.."background.png"
+--theme.wallpaper = "~/.config/awesome/themes/default/background.png"
 
 -- You can use your own layout icons like this:
-theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
-theme.layout_fairv = themes_path.."default/layouts/fairvw.png"
-theme.layout_floating  = themes_path.."default/layouts/floatingw.png"
-theme.layout_magnifier = themes_path.."default/layouts/magnifierw.png"
-theme.layout_max = themes_path.."default/layouts/maxw.png"
-theme.layout_fullscreen = themes_path.."default/layouts/fullscreenw.png"
-theme.layout_tilebottom = themes_path.."default/layouts/tilebottomw.png"
-theme.layout_tileleft   = themes_path.."default/layouts/tileleftw.png"
-theme.layout_tile = themes_path.."default/layouts/tilew.png"
-theme.layout_tiletop = themes_path.."default/layouts/tiletopw.png"
-theme.layout_spiral  = themes_path.."default/layouts/spiralw.png"
-theme.layout_dwindle = themes_path.."default/layouts/dwindlew.png"
-theme.layout_cornernw = themes_path.."default/layouts/cornernww.png"
-theme.layout_cornerne = themes_path.."default/layouts/cornernew.png"
-theme.layout_cornersw = themes_path.."default/layouts/cornersww.png"
-theme.layout_cornerse = themes_path.."default/layouts/cornersew.png"
+theme.layout_fairh = theme_path.."layouts/fairhw.png"
+theme.layout_fairv = theme_path.."layouts/fairvw.png"
+theme.layout_floating  = theme_path.."layouts/floatingw.png"
+theme.layout_magnifier = theme_path.."layouts/magnifierw.png"
+theme.layout_max = theme_path.."layouts/maxw.png"
+theme.layout_fullscreen = theme_path.."layouts/fullscreenw.png"
+theme.layout_tilebottom = theme_path.."layouts/tilebottomw.png"
+theme.layout_tileleft   = theme_path.."layouts/tileleftw.png"
+theme.layout_tile = theme_path.."layouts/tilew.png"
+theme.layout_tiletop = theme_path.."layouts/tiletopw.png"
+theme.layout_spiral  = theme_path.."layouts/spiralw.png"
+theme.layout_dwindle = theme_path.."layouts/dwindlew.png"
+theme.layout_cornernw = theme_path.."layouts/cornernww.png"
+theme.layout_cornerne = theme_path.."layouts/cornernew.png"
+theme.layout_cornersw = theme_path.."layouts/cornersww.png"
+theme.layout_cornerse = theme_path.."layouts/cornersew.png"
 
 -- Define the icon theme for application icons. If not set then the icons
 -- from /usr/share/icons and /usr/share/icons/hicolor will be used.
