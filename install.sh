484d0ca1fcedb422c16b264ea77fc23d309a3b21
Vinnie Magro <vmagro@fb.com>
2019-01-09 12:27:15 -0800

    symlink binaries

--- install.sh
+++ install.sh
@@ -7,6 +7,10 @@ set -x
 ln -f -s "$DIR/nvim" "$HOME/.config/nvim"
 ln -f -s "$DIR/tmux.conf" "$HOME/.tmux.conf"
 
+mkdir "$HOME/bin"
+ln -f -s "$DIR/bin/dev_maintenance" "$HOME/bin/dev_maintenance"
+ln -f -s "$DIR/bin/fzfwm.sh" "$HOME/bin/fzfwm"
+
 ln -f -s "$DIR/prezto" "$HOME/.zprezto"
 ln -f -s "$DIR/prezto/runcoms/zshrc" "$HOME/.zshrc"
 ln -f -s "$DIR/prezto/runcoms/zlogin" "$HOME/.zlogin"
